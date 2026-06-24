043aa66e19284c2b21f50bf0366ffd41003126c9
Supriya <supriyapaudel296@gmail.com>
2026-06-24 11:31:42 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function Title(){
+  return(
+    <div>
+    <h1>Supriya this side</h1>
+    <p>Use this site for reference</p>
+    </div>
+
+  )
+}
+
+export default function App(){
+
+  return (<Title/>)
+
+}
